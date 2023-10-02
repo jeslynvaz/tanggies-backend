b768c5f820c221766a342dffa5ac14d9eedf48f4
markpereira1 <49989116+markpereira1@users.noreply.github.com>
2023-10-03 03:51:19 +0530

    completed Product Service

--- src/main/java/com/tanggiesbackend/entities/Category.java
+++ src/main/java/com/tanggiesbackend/entities/Category.java
@@ -3,12 +3,14 @@ package com.tanggiesbackend.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Builder
 @Data
+@AllArgsConstructor
 public class Category {
 
     @Id
