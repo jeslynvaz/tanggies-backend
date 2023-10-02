13599a2965af99dc22ad44eaab9d2a7c6d97499e
Savio_Abel_Fernandes <26savio05@gmail.com>
2023-10-03 00:21:45 +0530

    initialized project

---
+++ src/main/java/com/tanggiesbackend/TanggiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tanggiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanggiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanggiesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanggiesbackend/TanggiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanggiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanggiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
